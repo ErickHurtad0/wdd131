06f47a527dd3481ff3efebe4f840151ce7b4aac7
ErickHurtad0 <3rickhurtado3@gmail.com>
2025-10-07 20:39:39 -0600

    Update theory.js

--- project/scripts/theory.js
+++ project/scripts/theory.js
@@ -112,6 +112,15 @@ nextBtn.addEventListener("click", () => {
 
 showQuestion();
 
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("burguer-menu");
+    const menu = document.getElementById("main-menu");
+
+    menuBtn.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+});
+
 const date = document.querySelector("#currentyear")
 
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
