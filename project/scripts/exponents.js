b3390aad00ce2337964876827c43cfc50d1de9a2
ErickHurtad0 <3rickhurtado3@gmail.com>
2025-10-07 20:39:21 -0600

    Update exponents.js

--- project/scripts/exponents.js
+++ project/scripts/exponents.js
@@ -25,6 +25,15 @@ document.getElementById("signupForm").addEventListener("submit", (e) => {
 });
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("burguer-menu");
+    const menu = document.getElementById("main-menu");
+
+    menuBtn.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+});
+
 const date = document.querySelector("#currentyear")
 
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
